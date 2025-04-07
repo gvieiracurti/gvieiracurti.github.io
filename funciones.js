0eb5bb9a603b6656191adef0ff5abc1bdd287508
Guillermo Vieira <vieira@ort.edu.uy>
2025-04-07 14:33:26 -0300

    agrgado estilos y funciones

---
+++ funciones.js
@@ -0,0 +1,12 @@
+
+onload=inicio;
+
+function inicio() {
+    document.getElementById("boton").addEventListener("click", test);
+    
+}
+
+function test() {
+    alert("Hola mundo");
+    console.log("Hola mundo");
+}
